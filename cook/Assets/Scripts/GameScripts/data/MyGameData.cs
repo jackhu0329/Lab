09d0af7c01becd904e7eaec469caf4619e976654
jackHu <jackhu0329@gmail.com>
2020-09-02 02:51:15 +0800

    add scripts editor

---
+++ cook/Assets/Scripts/GameScripts/data/MyGameData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DataSync;
+
+namespace LabData
+{
+    public class MyGameData : LabDataBase
+    {
+        public string hand { get; set; }
+        public int angle { get; set; }
+        public int dishCount { get; set; }
+
+        public MyGameData()
+        {
+            hand = "right";
+            angle = 30;
+            dishCount = 5;
+        }
+        public MyGameData(string handValue,int angleValue,int dishCountValue)
+        {
+            hand = handValue;
+            angle = angleValue;
+            dishCount = dishCountValue;
+        }
+
+    }
+}
+
--- cook/Assets/Scripts/LabFrameRelease/GameUi/MainUI.cs
+++ cook/Assets/Scripts/LabFrameRelease/GameUi/MainUI.cs
@@ -12,14 +12,17 @@ public class MainUI : MonoBehaviour
     public Button startButton;
     public Button settingButton;
     public Button settingFinishButton;
-    public Dropdown hand;
+    public Dropdown hand,angle,dish;
+    public InputField scriptName;
 
     public void Start()
     {
+        LabTools.CreateDataFolder<MyGameData>();
         launcher.SetActive(true);
         editor.SetActive(false);
         startButton.onClick.AddListener(StartButtonClick);
         settingButton.onClick.AddListener(SettingButtonClick);
+        settingFinishButton.onClick.AddListener(SettingFinishButtonClick);
     }
 
     public void StartButtonClick()
@@ -43,7 +46,66 @@ public class MainUI : MonoBehaviour
 
     public void SettingFinishButtonClick()
     {
+        MyGameData gameData = new MyGameData();
+        SetData(gameData);
+
+        if (scriptName.text != null || scriptName.text != "")
+        {
+            LabTools.WriteData(gameData, scriptName.text, true);
+        }
+        else
+        {
+            LabTools.WriteData(gameData, "default", true);
+        }
+        //LabTools.WriteData(gameData, "test123",true);
+        Debug.Log("dropdown data:" + gameData.hand);
+        Debug.Log("dropdown data:" + gameData.angle);
+        Debug.Log("dropdown data:" + gameData.dishCount);
         launcher.SetActive(true);
         editor.SetActive(false);
     }
+
+    private void SetData(MyGameData data)
+    {
+        if(hand.value == 0)
+        {
+            data.hand = "right";
+        }
+        else if (hand.value ==1)
+        {
+            data.hand = "left";
+        }
+
+        if(angle.value == 0)
+        {
+            data.angle = 10;
+        }
+        else if (angle.value == 1)
+        {
+            data.angle = 30;
+        }
+        else if (angle.value == 2)
+        {
+            data.angle = 90;
+        }
+        else if (angle.value == 3)
+        {
+            data.angle = 180;
+        }
+
+        if (dish.value == 0)
+        {
+            data.dishCount = 5;
+        }
+        else if (dish.value == 1)
+        {
+            data.dishCount = 10;
+        }
+        else if (dish.value == 2)
+        {
+            data.dishCount = 15;
+        }
+    }
+
+    
 }
